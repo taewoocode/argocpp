70eec2f3588883d41079f70c44c6e0f0a1d1eada
taewoocode <qkrxodn6035@gmail.com>
2024-03-01 23:27:43 +0900

    vector

--- e.cpp
+++ e.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    int a[] = {1, 2, 3};
+    vector<int> a = {2,1,3};
+    sort(a.begin(), a.end());
     do{
         for(int i : a) cout << i << " ";
         cout << '\n';
-    }while(next_permutation(&a[0], &a[1] + 3 ));
+    }while(next_permutation(a.begin(), a.end()));
 }

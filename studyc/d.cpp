2617e826d6a1548c78e7d634949531d1aa078d6d
taewoocode <qkrxodn6035@gmail.com>
2024-05-17 11:53:40 +0900

    bufferflash

---
+++ studyc/d.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T;
+string s;
+int main()
+{
+    cin >> T;
+    string bufferflash;
+    getline(cin, bufferflash);
+    for (int i = 0; i < T; i++)
+    {
+        cout << s << "\n";
+    }
+    return 0;
+}
---
+++ studyc/d.md
@@ -0,0 +1,2 @@
+g++ -std=c++14 -Wall d.cpp -o test.out
+./test.out

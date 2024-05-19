9a3fd2b965736a5d08b4e685bb33e7b5e19e1373
taewoocode <qkrxodn6035@gmail.com>
2024-05-19 18:07:00 +0900

    배열의 주소 추가

--- 5.19/f.cpp
+++ 5.19/f.cpp
@@ -5,8 +5,14 @@ int main()
     string a = "kundol";
     string *b = &a; // a의 주소값을 가르키는 포인터 b 선언
     cout << b << '\n';
-    cout << *b << '\n';
-    cout << *b << '\n';
+    cout << *b << '\n'; // 역참조 연산자
+
+    int arr[3] = {1, 2, 3};
+    int *c = arr;
+    cout << c << '\n';
+    cout << &arr[0] << '\n';
+    cout << c + 1 << '\n';
+    cout << &arr[1] << '\n';
 
     return 0;
 }

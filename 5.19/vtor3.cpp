6b6dc445d40fdb383887edef6934cf606c26ea59
taewoocode <qkrxodn6035@gmail.com>
2024-05-19 19:56:30 +0900

    unique함수 사용

--- 5.19/vtor3.cpp
+++ 5.19/vtor3.cpp
@@ -1,13 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> v {1,1,2,2,3,3,5,6,7,8,9};
+vector<int> v = {2,2,1,1,2,2,3,3,5,6,7,8,9};
 int main(){
-    auto it = unique(v.begin(), v.end());
-    for(int i : v) cout << i << " "; 
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+    for(int i : v ) cout << i << " ";
     cout << '\n';
     return 0;
 }
 
+
 /*
 1 2 3 5 6 7 8 9 7 8 9
 */
+
+

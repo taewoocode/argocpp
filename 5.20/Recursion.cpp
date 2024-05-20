a49c2c8e9e217f328d431bc50111b7aa4dbd6411
taewoocode <qkrxodn6035@gmail.com>
2024-05-21 01:08:17 +0900

    fibo, fact

---
+++ 5.20/Recursion.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n){
+    if(n == 1 || n == 0) return 1; //기저사례: 1이거나 0이면 둘중에 하나라도 맞으면 return
+    return n * fact(n - 1);
+}
+
+int f(int n){
+    if(n == 100) return 100; //기저사례: 상한선을 정해서 종료시켜 버림
+    return f(n+1);
+}
+
+int fibo(int n){
+    if(n == 0 || n == 1) return 1;
+    return fibo(n-1) + fibo(n-2);
+}
+
+int n = 5;
+int main(){
+    cout << fact(n) << " " << fibo(n) << '\n';
+    return 0;
+}

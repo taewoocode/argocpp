e5370147267fc91623dc756efaffbb6f83651036
taewoocode <qkrxodn6035@gmail.com>
2024-05-29 17:43:58 +0900

    push

---
+++ 5.29/d.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> array {1,2,3,4,5};
+    int v = array.size();
+    cout << v << '\n';
+    return 0;
+}
---
+++ 5.29/e.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> solution(int money){
+    vector<int> answer;
+    answer.push_back(money / 5500);
+    answer.push_back(money % 5500);
+
+    return answer;
+}
+
+
+
+int main(){
+    vector<int> money = solution(5500);
+    for(auto i : money){
+        cout << i << ' ';
+    }
+
+    return 0;
+}

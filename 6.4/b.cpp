54d90239214d092ed58c50164f4aca02894d19c4
taewoocode <qkrxodn6035@gmail.com>
2024-06-04 13:42:37 +0900

    asd

--- 6.4/b.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(vector<int> numbers, int num1, int num2){
-    vector<int> answer;
-    for(int i = num1; i <= num2; i++){
-        answer.push_back(numbers[i]);
-    }
-    
-}

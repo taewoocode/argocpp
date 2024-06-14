41be8e90047a72e69320b12a59887e87f3fe47c4
taewoocode <qkrxodn6035@gmail.com>
2024-06-14 22:08:14 +0900

    left shift

--- 6.14/b.cpp
+++ 6.14/b.cpp
@@ -1,10 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int S = 18;
-    int idx = 1;
-    S &= ~(1 << idx);
+    int S = 18; //10010
+    int idx = 1; 
+    S &= ~(1 << idx); //10010 << leftshift 1번째 비트를 왼쪽으로 1칸 이동시켜라 
     cout << S << '\n';
-
     return 0;
 }
+
+/*
+만약 idx값이 2였다면 왼쪽으로 2칸을 이동시키게 된다.
+
+
+*/

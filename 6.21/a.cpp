34c86eddc1a27fb705e2c42d5cad9f3a198ed315
taewoocode <qkrxodn6035@gmail.com>
2024-06-21 22:09:27 +0900

    bit

---
+++ 6.21/a.cpp
@@ -0,0 +1,13 @@
+// 10010 -> 10011 s |= (1<<idx)
+// 1<<0 -> 00001
+//or연산자는 00빼고는 다 괜찮다.
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int s = 18;
+    int idx = 0;
+    s |= (1 << idx); //0번째 비트를 킨다.
+    cout << s << '\n';
+    return 0;
+
+}

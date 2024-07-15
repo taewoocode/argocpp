a442e6b3e3d35d7ad6651d2d5513c4762c13514e
taewoocode <qkrxodn6035@gmail.com>
2024-07-15 21:51:30 +0900

    asd

--- 7.15/vector.cpp
+++ 7.15/vector.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2){
-    vector<vector<int>> answer(arr1.size(), vector<int>(arr1[0].size(), 0));
+    vector<vector<int>> answer(arr1.size(), vector<int>(arr1[0].size(), 0)); //크기, 0으로 초기화asd
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);

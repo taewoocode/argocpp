c3604e99e52beb3659f9a0e682db8941cc6a157f
taewoocode <qkrxodn6035@gmail.com>
2024-08-05 22:12:46 +0900

    프로그래머스 문제풀이

---
+++ 8.5/aa.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b){
+    if(a % b == 0) return b;
+    else return gcd(b, a % b);
+}
+
+int lcm(int a, int b){
+    return a * b / gcd(a,b);
+}
+
+vector<int> solution(int n, int m){
+    vector<int> answer;
+    answer.push_back(gcd(n,m));
+    answer.push_back(lcm(n,m));
+
+    return answer;
+}

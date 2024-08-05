04f98073eea976dd4d1b0ad65eb571b5aaf3567a
taewoocode <qkrxodn6035@gmail.com>
2024-08-05 22:40:03 +0900

    프로그래머스 예산

---
+++ 8.5/c.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(), d.end());
+    for(int i = 0; i < d.size(); i++){
+        if(budget <d[i]){
+            break;
+        } else{
+            budget -= d[i];
+            answer++;
+        }
+    }
+    
+    return answer;
+}

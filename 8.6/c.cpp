9754d36611f2ba62b535eaa7e7cf19223a261fc0
taewoocode <qkrxodn6035@gmail.com>
2024-08-06 19:02:48 +0900

    아스키코드

---
+++ 8.6/c.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string s = "123";
+    s[1]++; //143
+    cout << s << '\n';
+    return 0;
+}

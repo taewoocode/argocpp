51fb5db549ca17659f218e661793f8144db6e171
taewoocode <qkrxodn6035@gmail.com>
2024-08-18 15:46:15 +0900

    백준10818

---
+++ 8.18/c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+    
+    vector<int> v(n); 
+
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];  
+    }
+    cout << *min_element(v.begin(), v.end()) << ' ' << *max_element(v.begin(), v.end()) << endl;
+
+    return 0;
+}

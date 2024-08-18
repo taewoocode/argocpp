05aa7d143cde9042bbee1ac14108df7a37ece4b9
taewoocode <qkrxodn6035@gmail.com>
2024-08-19 00:04:08 +0900

    백준 문제풀이

--- 8/8.18/2798.cpp
+++ 8/8.18/2798.cpp
@@ -15,7 +15,8 @@ int main(){
     ret = 0;
     for(int i = 0; i < n - 2; i++){
         for(int j = i + 1; j < n - 1; j++){
-            for(int k = j + 1; k < j; k++){
+            for(int k = j + 1; k < n; k++){
+                int sum = v[i] + v[j] + v[k];
                 if(sum <= m){
                     ret = max(ret, sum);
                 }

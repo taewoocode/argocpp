807220762cb87f6a5f4519565d5d6d26f5349549
taewoocode <qkrxodn6035@gmail.com>
2024-09-18 06:59:50 +0900

    입출력 올리기

--- 9/9.18/a.cpp
+++ 9/9.18/a.cpp
@@ -1,8 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 //prefixsum
+/*
+구간합은 정적배열을 이용한다.
+*/
 int a[100004], psum[100004],n,m,b,c;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     cin >> n >> m;
     for(int i = 0; i < n; i++){
         cin >> a[i];
